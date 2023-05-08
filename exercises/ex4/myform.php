48551b18642abd9f06ededdabdb2beb236df8e30
omersela1 <omer.sela2510@gmail.com>
2023-05-08 12:32:48 +0300

    Update myform.php

--- exercises/ex4/myform.php
+++ exercises/ex4/myform.php
@@ -7,6 +7,6 @@
         <h1>Edited order: </h1>
         <h3>Color: <?php echo $_GET["color"]; ?></h3>
         <h3>Size: <?php echo $_GET["size"]; ?></h3>
-        <h3>Delivery address: <?php echo $_GET["model"] ?></h3>
+        <h3>Delivery address: <?php echo $_GET["address"] ?></h3>
     </body>
 </html>
